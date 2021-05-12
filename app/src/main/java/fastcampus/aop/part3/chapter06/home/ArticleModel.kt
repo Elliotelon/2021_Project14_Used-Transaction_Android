44215cc1b33282e4b209b39069b8cad0c4dacce4
kimminkyu <kmk50009@naver.com>
2021-05-13 00:57:17 +0900

    Used Transaction 상품 목록 페이지 UI 구성

---
+++ app/src/main/java/fastcampus/aop/part3/chapter06/home/ArticleModel.kt
@@ -0,0 +1,9 @@
+package fastcampus.aop.part3.chapter06.home
+
+data class ArticleModel(
+    val sellerId: String,
+    val title: String,
+    val createdAt: Long,
+    val price: String,
+    val imageUrl: String
+)
--- app/src/main/java/fastcampus/aop/part3/chapter06/home/HomeFragment.kt
+++ app/src/main/java/fastcampus/aop/part3/chapter06/home/HomeFragment.kt
@@ -1,8 +1,33 @@
 package fastcampus.aop.part3.chapter06.home
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import fastcampus.aop.part3.chapter06.R
+import fastcampus.aop.part3.chapter06.databinding.FragmentHomeBinding
 
 class HomeFragment: Fragment(R.layout.fragment_home) {
+
+    private var binding: FragmentHomeBinding? = null
+    private lateinit var articleAdapter: ArticleAdapter
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val fragmentHomeBinding = FragmentHomeBinding.bind(view)
+        binding = fragmentHomeBinding
+
+        articleAdapter = ArticleAdapter()
+        articleAdapter.submitList(mutableListOf<ArticleModel>().apply {
+            add(ArticleModel("0", "aaa", 1000000, "5000원", ""))
+
+        })
+
+        fragmentHomeBinding.articleRecyclerView.layoutManager = LinearLayoutManager(context)
+        fragmentHomeBinding.articleRecyclerView.adapter = articleAdapter
+
+    }
 
 }
