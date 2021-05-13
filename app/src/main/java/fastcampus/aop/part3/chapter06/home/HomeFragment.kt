38744ab255811172e42be95a37d410b0d250fb1a
kimminkyu <kmk50009@naver.com>
2021-05-13 11:30:34 +0900

    Used Transaction 물품등록 페이지 구현

--- app/src/main/java/fastcampus/aop/part3/chapter06/home/HomeFragment.kt
+++ app/src/main/java/fastcampus/aop/part3/chapter06/home/HomeFragment.kt
@@ -1,10 +1,11 @@
 package fastcampus.aop.part3.chapter06.home
 
+import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.database.ChildEventListener
@@ -61,6 +62,19 @@ class HomeFragment: Fragment(R.layout.fragment_home) {
 
         fragmentHomeBinding.articleRecyclerView.layoutManager = LinearLayoutManager(context)
         fragmentHomeBinding.articleRecyclerView.adapter = articleAdapter
+
+        fragmentHomeBinding.addFloatingButton.setOnClickListener {
+            context?.let{
+                //if(auth.currentUser != null){
+                    val intent = Intent(it, AddArticleActivity::class.java)
+                    startActivity(intent)
+                //} else {
+                //    Snackbar.make(view, "로그인 후 사용해주세요", Snackbar.LENGTH_LONG).show()
+                //}
+
+            }
+
+        }
 
         articleDB.addChildEventListener(listener)
     }
